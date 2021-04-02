ec2e929c8def0cdc3ae07bdc10de6f710255bb53
iburtakov <burtakov.ia@phystech.edu>
2021-04-02 12:45:19 +0300

    x

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -1,187 +1,251 @@
-#include <limits>
+#define _USE_MATH_DEFINES
 #include <cmath>
+#include <limits>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstring>
+#include <algorithm>
 #include "geometry.h"
+#include <mpi.h>
+// #define _BMP_H_
+// #include "bmp.h"
+
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+
+const float EPSILON = 0.0000001;
+const float AMBIENT_COEFF = 0.01;
+const float SPECULAR_COEFF = 0.5, SPECULAR_POW = 100;
+const float DIFF_BASE = 60, DIFF_COEFF = 0.5;
+
+const int DEPTH = 3;
+
+void save_image(char* filename, int w, int h, unsigned char* data) {
+	const int channels_num = 1;
+	stbi_write_jpg(filename, w, h, channels_num, data, 100);
+}
 
 struct Light {
-    Light(const Vec3f &p, const float &i) : position(p), intensity(i) {}
+    Light(const Vec3f &p, const float &i, const float &ID, const float &IM, const float &IA)
+     : position(p), intensity(i), id(ID), is(IM), ia(IA) {}
     Vec3f position;
     float intensity;
+    float id;
+    float is;
+    float ia;
+
 };
-
-
-struct Material {
-    Material(const Vec2f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular_exponent(spec) {}
-    Material() : albedo(1,0), diffuse_color(), specular_exponent() {}
-    Vec2f albedo;
-    Vec3f diffuse_color;
-    float specular_exponent;
-};
-
-// struct Sphere {
-//     Vec3f center;
-//     float radius;
-//     Material material;
-
-//     Sphere(const Vec3f &c, const float &r, const Material &m) : center(c), radius(r), material(m) {}
-
-//     bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
-//         Vec3f L = center - orig;
-//         float tca = L*dir;
-//         float d2 = L*L - tca*tca;
-//         if (d2 > radius*radius) return false;
-//         float thc = sqrtf(radius*radius - d2);
-//         t0       = tca - thc;
-//         float t1 = tca + thc;
-//         if (t0 < 0) t0 = t1;
-//         if (t0 < 0) return false;
-//         return true;
-//     }
-// };
 
 Vec3f reflect(const Vec3f &I, const Vec3f &N) {
     return I - N*2.f*(I*N);
 }
 
-
-struct Triangle{
+struct Triangle
+{
     const float EPSILON = 1e-8;
     Vec3f vert0;
     Vec3f vert1;
     Vec3f vert2;
-    Material material;
+    Vec3f normal;
 
-    Triangle(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2, const Material &m) : vert0(v0), vert1(v1), vert2(v2), material(m) {}
+  Triangle(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2) : vert0(v0), vert1(v1), vert2(v2) 
+  {
+    normal = cross(v1 - v0, v2 - v0).normalize();
+  }
+  //Moller - Trumbor algorithm
+  bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t, int& transparent) const 
+  {
+    float u; float v; float w;
+    Vec3f edge1 = vert1 - vert0;
+    Vec3f edge2 = vert2 - vert0;
 
-    bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
-        float u; float v; float w;
-        Vec3f edge1 = vert1 - vert0;
-        Vec3f edge2 = vert2 - vert0;
+    Vec3f pvec = cross(dir, edge2);
 
-        Vec3f pvec = cross(dir, edge2);
+    float det = edge1 * pvec;
 
-        float det = edge1 * pvec;
+    if (det < EPSILON)
+        return false;
+    float inv_det = 1.0f / det;
 
-        if (det < EPSILON)
-            return false;
-        float inv_det = 1.0f / det;
+    Vec3f tvec = orig - vert0;
 
-        Vec3f tvec = orig - vert0;
+    u = (tvec * pvec) * inv_det;
 
-        u = (tvec * pvec) * inv_det;
+    if (u < 0.0 || u > 1.0f)
+        return false;
 
-        if (u < 0.0 || u > 1.0f)
-            return false;
+    Vec3f qvec = cross(tvec, edge1);
 
-        Vec3f qvec = cross(tvec, edge1);
+    v = (dir * qvec) * inv_det;
+    if (v < 0.0 || u + v > 1.0f)
+        return false;
 
-        v = (dir * qvec) * inv_det;
-        if (v < 0.0 || u + v > 1.0f)
-            return false;
-
-        t = (edge2 * qvec) * inv_det;
-
-        return true;
+    t = (edge2 * qvec) * inv_det;
+    if (t > 0.0)
+    {
+      float s = (dir - orig) * normal;
+      transparent = s < 0.0 ? -1 : 1;
     }
+    return true;
+  }
 };
 
-bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Triangle> &triangles, Vec3f &hit, Vec3f &N, Material &material) {
-    float triangles_dist = std::numeric_limits<float>::max();
-    for (size_t i=0; i < triangles.size(); i++) {
-        float dist_i;
-        if (triangles[i].ray_intersect(orig, dir, dist_i) && dist_i < triangles_dist) {
-            triangles_dist = dist_i;
-            hit = orig + dir*dist_i;
-            //N = (hit - spheres[i].center).normalize();
-            N = (cross(triangles[i].vert1 - triangles[i].vert0, triangles[i].vert2 - triangles[i].vert0)).normalize();
-            // if (N * hit < 0)
-            //     N = N * (-1);
-            material = triangles[i].material;
+void read_triangles(std::string file_name, std::vector<Triangle>& triangles, int size)
+{
+  std::ifstream inf(file_name);
+  Vec3f a,b,c;
+
+  while (inf && size--)
+  {
+    inf >> a.x >> a.y >> a.z;
+    inf >> b.x >> b.y >> b.z;
+    inf >> c.x >> c.y >> c.z;
+    triangles.push_back(Triangle(a,b,c));
+  }
+  return;
+}
+
+void read_lights(std::string file_name, std::vector<Triangle>& triangles, int size)
+{
+  std::ifstream inf(file_name);
+  Vec3f a,b,c;
+
+  while (inf && size--)
+  {
+    inf >> a.x >> a.y >> a.z;
+    inf >> b.x >> b.y >> b.z;
+    inf >> c.x >> c.y >> c.z;
+    triangles.push_back(Triangle(a,b,c));
+  }
+
+  return;
+}
+
+void read_lights(std::string file_name, std::vector<Light>& lights, int size)
+{
+  std::ifstream inf(file_name);
+  Vec3f a;
+  float inten, im, id, ia;
+
+  while (inf && size--)
+  {
+    inf >> a.x >> a.y >> a.z >> inten >> im >> id >> ia;
+    lights.push_back(Light(a, inten, im, id, ia));
+  }
+
+  return;
+}
+
+float cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Triangle> &triangles, const std::vector<Light> &lights, int recursion)
+{
+  if (recursion > DEPTH)
+    return 0.0;
+  recursion++;
+
+  float ret = 0.0;
+  float dist = 0.0;
+  int transparent = 0;
+  float sum_ia = 0.0;
+
+
+  for (size_t i = 0; i < triangles.size(); i++)
+  {
+    bool intersects = triangles[i].ray_intersect(orig, dir, dist, transparent);
+    Vec3f point = orig + dir * dist; //intersects point with light beam (from camera through pixel) and trangle(i) 
+
+    if (intersects)
+    {
+      if (transparent == 1) 
+      { //it means the ray goes through and finds other triangles
+        for (size_t j = 0; j < lights.size(); j++)
+        {
+          sum_ia += lights[j].ia;
+          float flare = powf(fabs((dir) * reflect(point - lights[j].position, triangles[i].normal).normalize()), SPECULAR_POW)* lights[j].is;
+          ret += SPECULAR_COEFF * flare + DIFF_COEFF * fabs(triangles[i].normal * (point - lights[j].position).normalize()) * lights[j].id + cast_ray(point, dir, triangles, lights, recursion);
         }
-    }
-    return triangles_dist<1000;
-}
-
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Triangle> &triangles, const std::vector<Light> &lights){
-    Vec3f point, N;
-    Material material;
-
-    if (!scene_intersect(orig, dir, triangles, point, N, material)){
-        return Vec3f(0.2, 0.7, 0.8); // background color
-    }
-    //return material.diffuse_color;
-    float diffuse_light_intensity = 0, specular_light_intensity = 0;
-    for (size_t i=0; i<lights.size(); i++) {
-        Vec3f light_dir      = (lights[i].position - point).normalize();
-        float light_distance = (lights[i].position - point).norm();
-        //shadows
-        Vec3f shadow_orig = light_dir*N < 0 ? point - N*1e-3 : point + N*1e-3; // checking if the point lies in the shadow of the lights[i]
-        Vec3f shadow_pt, shadow_N;
-        Material tmpmaterial;
-        if (scene_intersect(shadow_orig, light_dir, triangles, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt-shadow_orig).norm() < light_distance)
-            continue;
-        
-        diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
-        specular_light_intensity += powf(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;
-    }
-    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1];
-}
-/* old cast_ray func
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Triangle &triangle) {
-    float triangle_dist = std::numeric_limits<float>::max();
-    float t = triangle_dist; //t = dist_i
-    if (!triangle.ray_intersect(orig, dir, t)) {
-        return Vec3f(0.2, 0.7, 0.8); // background color
-    }
-
-    return Vec3f(0.4, 0.4, 0.3);
-}
-*/
-void render(const std::vector<Triangle> &triangles, const std::vector<Light> &lights) {
-    const int width    = 1024;
-    const int height   = 768;
-    const int fov      = M_PI/2.;
-    std::vector<Vec3f> framebuffer(width*height);
-
-    #pragma omp parallel for
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
-            float x =  (2*(i + 0.5)/(float)width  - 1)*tan(fov/2.)*width/(float)height;
-            float y = -(2*(j + 0.5)/(float)height - 1)*tan(fov/2.);
-            Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), dir, triangles, lights);
+      }
+      else
+      { //triangle reflects
+        for (size_t j = 0; j < lights.size(); j++)
+        {
+          sum_ia += lights[j].ia;
+          float flare = powf(fabs((dir) * reflect(point - lights[0].position, triangles[i].normal).normalize()), SPECULAR_POW) * lights[j].is;
+				  ret += SPECULAR_COEFF * flare + DIFF_COEFF * fabs(triangles[i].normal * (point - lights[0].position).normalize()) * lights[j].id + cast_ray(point, reflect(dir, triangles[i].normal), triangles, lights, recursion);
         }
+      }
     }
-
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        Vec3f &c = framebuffer[i];
-        float max = std::max(c[0], std::max(c[1], c[2]));
-        if (max>1) c = c*(1./max);
-        for (size_t j = 0; j<3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
-    }
-    ofs.close();
+  }
+  return ret + sum_ia * AMBIENT_COEFF;
 }
 
-int main() {
-    Material      ivory(Vec2f(0.6,  0.3), Vec3f(0.4, 0.4, 0.3),   50.);
-    Material red_rubber(Vec2f(0.9,  0.1), Vec3f(0.3, 0.1, 0.1),   50.);
-    //Sphere sphere(Vec3f(-3, 0, -16), 2);
 
-    std::vector<Triangle> triangles;
+
+void render(const std::vector<Triangle> &triangles, const std::vector<Light> &lights, int argc, char* argv[]) 
+{
+    const int width    = 1400;
+    const int height   = 1000;
+    const int fov      = 3.14159 / 1.6;
+
+		int size, rank;
+
+		MPI_Init(&argc, &argv);
+		MPI_Comm_size(MPI_COMM_WORLD, &size);
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		double time;
+
+    unsigned char data[height * width];
+    memset(data, 0,height * width);
+
+    Vec3f dir, camera(12, 1, -7), rotation(-1, 0, -0.25);
+    float brightness = 0.0;
+
+		if (rank == 0) 
+    {
+			std::cout << "Cluster size: " << size << std::endl;
+			std::cout << "Triangles amount: " << triangles.size() << std::endl;
+			time = MPI_Wtime();
+		}
+
+    for (size_t j = height / size * rank; j < height / size * (rank + 1); j++)
+    {
+      for (size_t i = 0; i < width; i++)
+      {
+        float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width / (float)height;
+        float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
+        dir = (Vec3f(x, y, 1) + rotation).normalize();
+
+        brightness = DIFF_BASE * (cast_ray(camera, dir, triangles, lights, 0));
+        brightness = brightness > 255 ? (char)255 : (char)brightness;
+        data[i + j * width] = brightness;;
+      }
+    }
     
-    triangles.push_back(Triangle(Vec3f(-3, -3, -15), Vec3f(3, -3, -10), Vec3f(0, 3, -7), red_rubber));
-    triangles.push_back(Triangle(Vec3f(-1, -3, -9.6), Vec3f(1, -3, -9.6), Vec3f(0, -1, -9.6), ivory));
+		MPI_Gather(&data[height / size * rank * width], height * width / size, MPI_CHAR, data, height * width / size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+		if (rank == 0) 
+    {
+      std::ofstream ofs;
+      ofs.open("./out.ppm");
+      ofs << "P5\n" << width << " " << height << "\n255\n";
+      for (size_t i = 0; i < height * width; ++i) 
+      {
+          ofs << data[i];
+      }
+      ofs.close();
+		  std::cout << "Time: " << MPI_Wtime() - time << std::endl;
+	  }
+		MPI_Finalize();
+}
+
+int main(int argc, char* argv[]) 
+{
+    std::vector<Triangle> triangles;
     std::vector<Light>  lights;
-    lights.push_back(Light(Vec3f(0, 0, -3), 4));
-
-    render(triangles, lights);
-
+    read_triangles("model.xyz", triangles, 25992);
+    read_lights("lights.xyz", lights, 1);
+    render(triangles, lights, argc, argv);
     return 0;
 }
